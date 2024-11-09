e26d516e9a9a0b09609f12fd8525cbb769c89049
Alexander D. Carl <alexddcarl@gmail.com>
2024-11-09 15:55:15 -0500

    added the ajax calls that gets it when you search for items

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,14 +1,52 @@
 $(document).ready(function () {
-    $.ajax({
-        ec69b1867be5be538320790d2d30cb1a91e06e52
-        url: 'https://api.example.com/data',
-        type: 'GET',
-        dataType: 'json',
-        success: function(response) {
-            console.log('Success:', response);
-        },
-        error: function(xhr, status, error) {
-            console.error('Error:', error);
-        }
-    });
+    function get_search(searchName) {
+        $.ajax({
+            url: `https://data.unwrangle.com/api/getter/?platform=amazon_search&search=${searchName}&country_code=us&page=1&api_key=ec69b1867be5be538320790d2d30cb1a91e06e52`,
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                console.log('Success:', response);
+            },
+            error: function (xhr, status, error) {
+                console.error('Error:', error);
+            }
+        });
+        $.ajax({
+            url: `https://data.unwrangle.com/api/getter/?platform=walmart_search&search=${searchName}&country_code=us&page=1&api_key=ec69b1867be5be538320790d2d30cb1a91e06e52`,
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                console.log('Success:', response);
+            },
+            error: function (xhr, status, error) {
+                console.error('Error:', error);
+            }
+        });
+        $.ajax({
+            url: `https://data.unwrangle.com/api/getter/?platform=costco_search&search=${searchName}&country_code=us&page=1&api_key=ec69b1867be5be538320790d2d30cb1a91e06e52`,
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                console.log('Success:', response);
+            },
+            error: function (xhr, status, error) {
+                console.error('Error:', error);
+            }
+        });
+        $.ajax({
+            url: `https://data.unwrangle.com/api/getter/?platform=samsclub_search=${searchName}&country_code=us&page=1&api_key=ec69b1867be5be538320790d2d30cb1a91e06e52`,
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                console.log('Success:', response);
+            },
+            error: function (xhr, status, error) {
+                console.error('Error:', error);
+            }
+        });
+    }
+
+    $('.search-button').on('click', () => {
+        get_search($('#search-input').val());
+    })
 });
