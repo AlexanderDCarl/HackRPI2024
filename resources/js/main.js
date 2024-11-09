9204cf44f5634dc102d49a3fb5e8eb71457e39f1
Alexander D. Carl <alexddcarl@gmail.com>
2024-11-09 16:18:46 -0500

    added the reason for having the api key public

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     function get_search(searchName) {
         $.ajax({
+            // I dont care about the api key it was free so imma push it
             url: `https://data.unwrangle.com/api/getter/?platform=amazon_search&search=${searchName}&country_code=us&page=1&api_key=ec69b1867be5be538320790d2d30cb1a91e06e52`,
             type: 'POST',
             dataType: 'json',
