354be4140d0bff2a94c927df8acadf71d3b2d3c4
Lukas Petervary <lukas.at.petervary@gmail.com>
2024-11-09 22:26:47 -0500

    more formatting stuff

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,15 +3,9 @@ const slides = document.querySelectorAll('.slide');
 const totalSlides = slides.length;
 
 function showNextSlide() {
-    // Hide current slide
     slides[currentSlide].classList.remove('active');
-
-    // Move to the next slide
     currentSlide = (currentSlide + 1) % totalSlides;
-
-    // Show the next slide
     slides[currentSlide].classList.add('active');
 }
 
-// Automatically cycle through slides every 3 seconds
 setInterval(showNextSlide, 5000);
