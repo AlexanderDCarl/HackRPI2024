ab5e5557a61c806f7447b881fed6bcd818762d2a
Alexander D. Carl <alexddcarl@gmail.com>
2024-11-09 20:50:04 -0500

    LUKAS STOP REMOVING THE JS AND THE ID FOR THE SEARCH BAR

--- resources/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.search-button').on('click', function() {
-        const searchTerm = $('#search-input').val();
-        if (searchTerm.trim() !== "") {
-            window.location.href=`./html/search?q=${searchTerm}`
-        }
-    });
-});
